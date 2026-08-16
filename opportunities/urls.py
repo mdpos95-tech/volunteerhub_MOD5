6f0c545a2448ef54a3e33b66c7331cafbf67a94e
mdpos95-tech <mdpos95@gmail.com>
2026-08-16 16:55:27 +0100

    Initial code for py and html link

---
+++ opportunities/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+app_name = "opportunities"
+urlpatterns = [
+    path("", views.home, name="home"),
+]
--- opportunities/views.py
+++ opportunities/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """Display the main landing page for the Community Volunteering Hub"""
+    return render(request,"opportunities/home.html")
