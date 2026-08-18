43ba0cc3da410fb7d51c82e85b5242d887bce07d
mdpos95-tech <mdpos95@gmail.com>
2026-08-18 17:54:34 +0100

    Add html for django admin function

--- opportunities/views.py
+++ opportunities/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Opportunity
 
-# Create your views here.
 def home(request):
     """Display the main landing page for the Community Volunteering Hub"""
-    return render(request,"opportunities/home.html")
+    opportunities = Opportunity.objects.filter(is_active=True).order_by("-date")
+    context = {
+        "opportunities": opportunities,
+    }
+    return render(request, "opportunities/home.html", context)
