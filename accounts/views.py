2c8b67f1445c27f83437a6d763fde9e5ca79ff97
mdpos95-tech <mdpos95@gmail.com>
2026-08-19 17:17:29 +0100

    Chore: fix login rooting issue

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.shortcuts import redirect, render
 
 def register(request):
@@ -6,6 +7,8 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!. You can now log in.")
             return redirect("accounts:login")
     else:
         form = UserCreationForm()
