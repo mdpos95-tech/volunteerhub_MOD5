b53aaf11493e8a0eb849677dbd80432cf5fa2c9f
mdpos95-tech <mdpos95@gmail.com>
2026-08-22 15:19:37 +0100

    add paths thru urls.py for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("apply/<int:opportunity_id>/", views.apply_for_opportunity, name="apply"),
     path("my-applications/", views.my_applications, name="my_applications"),
     path ("profile/", views.profile, name="profile"),
+    path ("password-reset/", auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="password_reset"),
+    path ("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_done"),
+    path ("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name="password_reset_confirm"),
+    path ("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), name="password_reset_complete"),
 ]
