d7c51e2eb0eff0ac976917cbc9c1e01421774d19
mdpos95-tech <mdpos95@gmail.com>
2026-08-22 18:13:36 +0100

    adding message model to models.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Message
+
 
 class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['recipient', 'subject', 'body']
+
+        widgets = {
+            'recipient': forms.Select(attrs={'class': 'form-select'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter subject'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your message here...'}) }
