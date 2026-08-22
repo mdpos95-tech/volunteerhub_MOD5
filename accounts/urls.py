1b979bedd74fe348d2c0cb1775fa958833355be5
mdpos95-tech <mdpos95@gmail.com>
2026-08-22 16:10:21 +0100

    adding/fixing paths in urls.py to allow local password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -10,8 +10,8 @@ urlpatterns = [
     path("apply/<int:opportunity_id>/", views.apply_for_opportunity, name="apply"),
     path("my-applications/", views.my_applications, name="my_applications"),
     path ("profile/", views.profile, name="profile"),
-    path ("password-reset/", auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="password_reset"),
+    path ("password-reset/", auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html",email_template_name="accounts/password_reset_email.html", success_url=reverse_lazy("accounts:password_reset_done"),), name="password_reset"),
     path ("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_done"),
-    path ("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name="password_reset_confirm"),
+    path ("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html",success_url=reverse_lazy("accounts:password_reset_complete"),), name="password_reset_confirm"),
     path ("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), name="password_reset_complete"),
 ]
