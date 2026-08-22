acd5c3c1a1b359bee5d0bd1c2a46408ce3cfc669
mdpos95-tech <mdpos95@gmail.com>
2026-08-22 17:37:14 +0100

    creating message model

--- accounts/models.py
+++ accounts/models.py
@@ -21,3 +21,16 @@ class Meta:
     ]    
 def __str__(self):
         return f"{self.user.username} - {self.opportunity.title}"
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    subject = models.CharField(max_length=200)
+    body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-sent_at']
+    def __str__(self):
+        return f"From: {self.sender.username} To: {self.recipient.username}"
